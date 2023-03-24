ea66d76e9108fa92737ab48736b22f9615513b19
hennyobay <ennyadebayo@yahoo.com>
2023-03-23 17:16:28 -0700

    task

--- variadic_functions.h
+++ variadic_functions.h
@@ -1,11 +1,10 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-	int _putchar(char c);
-	int sum_them_all(const unsigned int n, ...);
-	void print_numbers(const char *separator, const unsigned int n, ...);
-	void print_strings(const char *separator, const unsigned int n, ...);
-	void print_all(const char * const format, ...);
-	#endif
-
+#endif
